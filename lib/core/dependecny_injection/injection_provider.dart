9531282bfa385379593f2d78c0b8e7aa1d725498
developerSajib88 <developersajib88@gmail.com>
2024-01-05 23:01:44 +0600

    Dependency Injection Complete

--- lib/core/dependecny_injection/injection_provider.dart
+++
@@ -1,6 +0,0 @@
-part of 'dependency_injection.dart';
-
-/// This is my riverpod state provider
-final videoPlayerProvider =
-StateNotifierProvider<VideoPlayerNotifier,VideoPlayerState>((ref) =>
-    VideoPlayerNotifier(videoPlayerDomain: singleTon()));
