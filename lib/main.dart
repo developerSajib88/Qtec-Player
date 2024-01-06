0fb1513b8feb383e140dbf3c3b8d5572a2073b8a
developerSajib88 <developersajib88@gmail.com>
2024-01-06 12:17:39 +0600

    1. Home screen presentation file create 2. Video view screen presentation file create 3. Slash screen create 4. Initialize anc configuration go router 5. Main method recreate and app.dart file create 6. app routes file create and code. 7. export create for presentation layer. 8. Dependency injection init.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:qtec_player/application/app.dart';
+import 'package:qtec_player/core/dependecny_injection/dependency_injection.dart' as di;
 
-void main(){
+void main()async{
   WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const ProviderScope(
       child: QtecPlayerApp()
    )
